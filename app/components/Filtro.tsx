befd54a93b8782c1684e0b6c6c78d6368d4bcadb
Tragu <dev.jandrade@gmail.com>
2025-10-12 19:43:06 -0300

    imagens de produtos

--- app/components/Filtro.tsx
+++ app/components/Filtro.tsx
@@ -1,8 +1,38 @@
+import './Filtro.scss';
+
 const Filtro: React.FC = () => {
     return (
         <div className="filtro-container">
             <h2>Filtro</h2>
             {/* Adicione aqui os elementos do filtro, como dropdowns, checkboxes, etc. */}
+            <form action="#">
+                <label>
+                    Marca:
+                </label>
+                <select>
+                    <option value="todas">Todas</option>
+                    <option value="marca1">Control ID</option>
+                    <option value="marca2">Henry</option>
+                    <option value="marca3">RW Tech</option>
+                    <option value="marca4">Top Data</option>
+                </select>
+                <label>Função: </label>
+                <br />
+
+                <input type="checkbox" />
+                <label>Biometria</label>
+
+                <input type="checkbox" />
+                <label>Cartão de proximidade</label>
+
+                <input type="checkbox" />
+                <label>Facial</label>
+
+                <input type="checkbox" />
+                <label>Senha</label>
+
+                <button>Desmarcar Todos</button>
+            </form>
         </div>
     );
 }
--- app/routes/Produtos.tsx
+++ app/routes/Produtos.tsx
@@ -3,6 +3,7 @@ import Header from '~/components/Header';
 import './Produtos.scss';
 import Item from '~/components/Item';
 import Filtro from '~/components/Filtro';
+import produtos from '~/data/produtos.json';
 
 const Produtos: React.FC = () => {
     return (
@@ -13,10 +14,10 @@ const Produtos: React.FC = () => {
                     <Filtro></Filtro>
                 </div>
                 <div className='produtos-list'>
-                    <Item title={'Produto 1'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac nisi volutpat, sollicitudin mauris ut, lacinia dui. Nulla maximus urna in odio congue eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam blandit neque posuere est elementum, vitae suscipit risus tempor. Nulla libero massa, mollis non nisl semper, euismod molestie odio. Morbi blandit augue quam, et semper elit vestibulum quis. Curabitur placerat, felis vitae tristique auctor, nisl ante ornare orci, non viverra eros quam sed sapien.'} image='/assets/item_1.jpg'></Item>
-                    <Item title={'Produto 2'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac nisi volutpat, sollicitudin mauris ut, lacinia dui. Nulla maximus urna in odio congue eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam blandit neque posuere est elementum, vitae suscipit risus tempor. Nulla libero massa, mollis non nisl semper, euismod molestie odio. Morbi blandit augue quam, et semper elit vestibulum quis. Curabitur placerat, felis vitae tristique auctor, nisl ante ornare orci, non viverra eros quam sed sapien.'} image='/assets/item_1.jpg'></Item>
-                    <Item title={'Produto 3'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac nisi volutpat, sollicitudin mauris ut, lacinia dui. Nulla maximus urna in odio congue eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam blandit neque posuere est elementum, vitae suscipit risus tempor. Nulla libero massa, mollis non nisl semper, euismod molestie odio. Morbi blandit augue quam, et semper elit vestibulum quis. Curabitur placerat, felis vitae tristique auctor, nisl ante ornare orci, non viverra eros quam sed sapien.'} image='/assets/item_1.jpg'></Item>
-                    <Item title={'Produto 4'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac nisi volutpat, sollicitudin mauris ut, lacinia dui. Nulla maximus urna in odio congue eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam blandit neque posuere est elementum, vitae suscipit risus tempor. Nulla libero massa, mollis non nisl semper, euismod molestie odio. Morbi blandit augue quam, et semper elit vestibulum quis. Curabitur placerat, felis vitae tristique auctor, nisl ante ornare orci, non viverra eros quam sed sapien.'} image='/assets/item_1.jpg'></Item>
+                    {/* Adicionar for para produtos. */}
+                    {produtos.map(produto => (
+                        <Item key={produto.id} title={produto.nome} description={produto.descricao} image={produto.img}></Item>
+                    ))}
                 </div>
             </div>
         </div>
